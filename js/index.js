392873aa9c5cf3b5937c93e2b8c9367f268c9086
Thauan Fernandes <thauan.fernandes@ufpe.br>
2022-04-28 15:19:39 -0300

    Upando

---
+++ js/index.js
@@ -0,0 +1,8 @@
+let listaDeFilmes = [
+    'https://2.bp.blogspot.com/-Hm5_gy7OX20/VLLnE-_VJeI/AAAAAAAAAs0/pxX6ZoRZpAY/s1600/Interestelar-download-filme-dublado-torrent.jpg',
+    'https://image.tmdb.org/t/p/w342/ojyrQRIQ6f0hHFpJoXTAeUx0ynX.jpg'
+]
+let listaDeTitulos = [
+    'Interestelar',
+    'Efeito Borboleta'
+]
---
+++ js/lacos.js
@@ -0,0 +1,14 @@
+for(let i = 0; i <= 10; i++){
+    console.log(`A tabuada do ${i} é:`);
+    for(let j = 0; j<= 10; j++){
+        console.log(`${i} * ${j} = ${i * j}`)
+    }
+}
+
+let a = 0
+while (a) {
+    
+}
+do {
+    
+} while (a<0);
